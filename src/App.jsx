7f9b4a323c41997d97a11df651e9a1ba97b319b7
ongheong <pudding4271@gmail.com>
2024-09-10 12:57:08 +0900

    💄 Feat: global style 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { css, Global} from '@emotion/react';
+import 'normalize.css';
+import SNSPage from './pages/SNSPage';
+
+//나중에 globalStyle.css로 분리하기
+const globalStyle = css`
+  html, body {
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+  }
+  * {
+    box-sizing: border-box;
+    scrollbar-width: none;
+    font-family: 'Roboto', sans-serif;
+  }
+  body {
+    display: block;
+  }
+`;
+
+const GlobalStyle = () => <Global styles={globalStyle} />;
+
 function App() {
-  return <div>React SNS</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <SNSPage />
+    </>
+  );
 }
 
 export default App;
