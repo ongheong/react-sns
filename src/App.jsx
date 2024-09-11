8281613c5d88865c1885398fce84bcadded4eac1
ongheong <pudding4271@gmail.com>
2024-09-11 10:09:54 +0900

    💄 Refactor: App.js의 Global 스타일 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { css, Global} from '@emotion/react';
+import { css, Global } from '@emotion/react';
 import 'normalize.css';
 import SNSPage from './pages/SNSPage';
 
 //나중에 globalStyle.css로 분리하기
 const globalStyle = css`
+  :root {
+    --border-color: #d9d9d9;
+    --other-bubble-color: #92A8D1;
+    --my-bubble-color: #f7cac9;
+    --main-text-color: #a6a6a6;
+    --other-text-color: #fff;
+    --my-text-color: #3a3a3a;
+    --vh: 9.34px;
+  }
   html, body {
-    overflow: hidden;
+    /* overflow: hidden; */
     margin: 0;
     padding: 0;
   }
@@ -13,9 +22,11 @@ const globalStyle = css`
     box-sizing: border-box;
     scrollbar-width: none;
     font-family: 'Roboto', sans-serif;
+    letter-spacing: -.4px;
   }
   body {
     display: block;
+    max-width: 500px;
   }
 `;
 
